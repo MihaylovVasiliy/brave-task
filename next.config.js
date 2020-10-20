504b8285e6e4e3f4161a64614627573a3345517d
Vasiliy-home <basen3012@gmail.com>
2020-10-20 04:09:43 +0400

    init commit #2: refactor project

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ require('dotenv').config()
 module.exports = {
   basePath: '',
   env: {
-    API_URL: process.env.API_URL,
-    API_URL2: process.env.API_URL2
+    API_URL: process.env.API_URL || ''
   }
 }
