1a2528304a98b9c3c17380787f80299c75afa874
Vasiliy-home <basen3012@gmail.com>
2020-10-20 04:19:32 +0400

    init commit #2: refactor project

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 require('dotenv').config()
 
 module.exports = {
-  basePath: '/brave-task.vercel.app',
   env: {
-    API_URL: process.env.API_URL || "https://brave-task.vercel.app"
+    API_URL: process.env.API_URL || process.env.API_VERCEL
   }
 }
